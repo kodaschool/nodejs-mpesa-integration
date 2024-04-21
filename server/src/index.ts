71ae3a0a14032796aa9adc346a6c25810e82e242
Mary <marieshix98@gmail.com>
2024-04-21 22:54:24 +0300

    file clean up

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,6 +6,7 @@ import router from "./routes/lipaRoute";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
+
 //middlewares
 app.use(express.json());
 app.use(cors());
@@ -18,28 +19,3 @@ app.use("/lipa", router);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-// let headers = new Headers();
-// headers.append("Content-Type", "application/json");
-// headers.append("Authorization", "Bearer IQg1mE9PK8EgHOIxAQOFdm9LCK6C");
-// fetch("https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest", {
-//   method: "POST",
-//   headers,
-//   body: JSON.stringify({
-//     BusinessShortCode: 174379,
-//     Password:
-//       "MTc0Mzc5YmZiMjc5ZjlhYTliZGJjZjE1OGU5N2RkNzFhNDY3Y2QyZTBjODkzMDU5YjEwZjc4ZTZiNzJhZGExZWQyYzkxOTIwMjQwNDIwMTkxMzEz",
-//     Timestamp: "20240420191313",
-//     TransactionType: "CustomerPayBillOnline",
-//     Amount: 1,
-//     PartyA: 254708374149,
-//     PartyB: 174379,
-//     PhoneNumber: 254708374149,
-//     CallBackURL: "https://mydomain.com/path",
-//     AccountReference: "CompanyXLTD",
-//     TransactionDesc: "Payment of X",
-//   }),
-// })
-//   .then((response) => response.text())
-//   .then((result) => console.log(result))
-//   .catch((error) => console.log(error));
