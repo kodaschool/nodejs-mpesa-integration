d8eff920ea4b1012e858a57baa9ad5e78c3401aa
Mary <marieshix98@gmail.com>
2024-04-21 22:53:10 +0300

    format timestamp

---
+++ server/utils/timeStamp.ts
@@ -0,0 +1,8 @@
+const date = new Date();
+export const timestamp =
+  date.getFullYear() +
+  ("0" + (date.getMonth() + 1)).slice(-2) +
+  ("0" + date.getDate()).slice(-2) +
+  ("0" + date.getHours()).slice(-2) +
+  ("0" + date.getMinutes()).slice(-2) +
+  ("0" + date.getSeconds()).slice(-2);
